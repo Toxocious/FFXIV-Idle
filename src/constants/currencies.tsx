6537d13f95ee711c24e031cef2061fbb4a2d3b3b
Jess <toxocious@gmail.com>
2022-03-23 20:01:54 -0700

    Set a list of valid currencies

---
+++ src/constants/currencies.tsx
@@ -0,0 +1,57 @@
+export const CURRENCIES = {
+  GIL: {
+    name: 'Gil',
+    value: 0,
+    imageName: 'gil',
+  },
+  MGP: {
+    name: 'Minigame Points',
+    value: 0,
+    imageName: 'mgp',
+  },
+
+  SERPENT_SEALS: {
+    name: 'Serpent Seals',
+    value: 0,
+    imageName: 'serpent_seals',
+  },
+  VENTURES: {
+    name: 'Ventures',
+    value: 0,
+    imageName: 'ventures',
+  },
+
+  ALLAGAN_TOMESTONE: {
+    name: 'Allagan Tomestone of Poetics',
+    value: 0,
+    imageName: 'tomestone_allagan',
+  },
+
+  SKYBUILDER_SCRIPS: {
+    name: "Skybuilders' Scrip",
+    value: 0,
+    imageName: 'skybuilder_scrip',
+  },
+
+  CRAFTER_SCRIP_YELLOW: {
+    name: "Yellow Crafter's Scrip",
+    value: 0,
+    imageName: 'crafter_scrip_yellow',
+  },
+  CRAFTER_SCRIP_WHITE: {
+    name: "White Crafter's Scrip",
+    value: 0,
+    imageName: 'crafter_scrip_white',
+  },
+
+  GATHERER_SCRIP_YELLOW: {
+    name: "Yellow Gatherer's Scrip",
+    value: 0,
+    imageName: 'gatherer_scrip_yellow',
+  },
+  GATHERER_SCRIP_WHITE: {
+    name: "White Gatherer's Scrip",
+    value: 0,
+    imageName: 'gatherer_scrip_white',
+  },
+};
