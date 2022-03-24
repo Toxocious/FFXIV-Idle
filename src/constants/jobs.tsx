1dc93c42f1db527a03e1fbe9b9ebca12cb44aa11
Jess <toxocious@gmail.com>
2022-03-23 20:02:07 -0700

    Set a list of valid FFXIV jobs

---
+++ src/constants/jobs.tsx
@@ -0,0 +1,255 @@
+export const JOBS = {
+  /**
+   * Tanks
+   */
+  GLA: {
+    name: 'Gladiator',
+    type: 'Tank',
+    level: 1,
+    iconName: 'icon-job-gla',
+  },
+  PLD: {
+    name: 'Paladin',
+    type: 'Tank',
+    level: 30,
+    iconName: 'icon-job-pld',
+  },
+  MRD: {
+    name: 'Marauder',
+    type: 'Tank',
+    level: 1,
+    iconName: 'icon-job-mrd',
+  },
+  WAR: {
+    name: 'Warrior',
+    type: 'Tank',
+    level: 30,
+    iconName: 'icon-job-war',
+  },
+  DRK: {
+    name: 'Dark Knight',
+    type: 'Tank',
+    level: 1,
+    iconName: 'icon-job-drk',
+  },
+  GNB: {
+    name: 'Gunbreaker',
+    type: 'Tank',
+    level: 60,
+    iconName: 'icon-job-gnb',
+  },
+
+  /**
+   * Healers
+   */
+  CNJ: {
+    name: 'Conjurer',
+    type: 'Healer',
+    level: 1,
+    iconName: 'icon-job-cnj',
+  },
+  WHM: {
+    name: 'White Mage',
+    type: 'Healer',
+    level: 30,
+    iconName: 'icon-job-whm',
+  },
+  SCH: {
+    name: 'Scholar',
+    type: 'Healer',
+    level: 1,
+    iconName: 'icon-job-sch',
+  },
+  AST: {
+    name: 'Astrologian',
+    type: 'Healer',
+    level: 30,
+    iconName: 'icon-job-ast',
+  },
+  SGE: {
+    name: 'Sage',
+    type: 'Healer',
+    level: 70,
+    iconName: 'icon-job-sge',
+  },
+
+  /**
+   * DPS
+   */
+  PGL: {
+    name: 'Pugilist',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-pgl',
+  },
+  MNK: {
+    name: 'Monk',
+    type: 'DPS',
+    level: 30,
+    iconName: 'icon-job-mnk',
+  },
+  LNC: {
+    name: 'Lancer',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-lnc',
+  },
+  DRG: {
+    name: 'Dragoon',
+    type: 'DPS',
+    level: 30,
+    iconName: 'icon-job-drg',
+  },
+  ROG: {
+    name: 'Rogue',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-rog',
+  },
+  NIN: {
+    name: 'Ninja',
+    type: 'DPS',
+    level: 30,
+    iconName: 'icon-job-nin',
+  },
+  ARC: {
+    name: 'Archer',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-arc',
+  },
+  BRD: {
+    name: 'Bard',
+    type: 'DPS',
+    level: 30,
+    iconName: 'icon-job-brd',
+  },
+  THM: {
+    name: 'Thaumaturge',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-thm',
+  },
+  BLM: {
+    name: 'Black Mage',
+    type: 'DPS',
+    level: 30,
+    iconName: 'icon-job-blm',
+  },
+  ACN: {
+    name: 'Arcanist',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-acn',
+  },
+  SMN: {
+    name: 'Summoner',
+    type: 'DPS',
+    level: 30,
+    iconName: 'icon-job-smn',
+  },
+  SAM: {
+    name: 'Samurai',
+    type: 'DPS',
+    level: 50,
+    iconName: 'icon-job-sam',
+  },
+  MCH: {
+    name: 'Machinist',
+    type: 'DPS',
+    level: 50,
+    iconName: 'icon-job-mch',
+  },
+  DNC: {
+    name: 'Dancer',
+    type: 'DPS',
+    level: 60,
+    iconName: 'icon-job-dnc',
+  },
+  RPR: {
+    name: 'Reaper',
+    type: 'DPS',
+    level: 70,
+    iconName: 'icon-job-rpr',
+  },
+  BLU: {
+    name: 'Blue Mage',
+    type: 'DPS',
+    level: 1,
+    iconName: 'icon-job-blu',
+  },
+
+  /**
+   * Crafters
+   */
+  CRP: {
+    name: 'Carpenter',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-crp',
+  },
+  BSM: {
+    name: 'Blacksmith',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-bs,',
+  },
+  ARM: {
+    name: 'Armorer',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-arm',
+  },
+  GSM: {
+    name: 'Goldsmith',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-gsm',
+  },
+  LTW: {
+    name: 'Leatherworker',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-ltw',
+  },
+  WVR: {
+    name: 'Weaver',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-wvr',
+  },
+  ALC: {
+    name: 'Alchemist',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-alc',
+  },
+  CUL: {
+    name: 'Culinarian',
+    type: 'Crafter',
+    level: 1,
+    iconName: 'icon-job-cul',
+  },
+
+  /**
+   * Gatherers
+   */
+  BTN: {
+    name: 'Botanist',
+    type: 'Gatherer',
+    level: 1,
+    iconName: 'icon-job-btn',
+  },
+  MIN: {
+    name: 'Miner',
+    type: 'Gatherer',
+    level: 1,
+    iconName: 'icon-job-min',
+  },
+  FSH: {
+    name: 'Fisher',
+    type: 'Gatherer',
+    level: 1,
+    iconName: 'icon-job-fsh',
+  },
+};
