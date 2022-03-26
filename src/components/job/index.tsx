9cdb9889591b302a0d1794f6025bbf1ec23fea9d
Jess <toxocious@gmail.com>
2022-03-25 20:09:20 -0700

    Implement component to display all jobs user has

---
+++ src/components/job/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useStore } from '../../context/game-context';
+
+import { JOBS } from '../../constants/jobs';
+
+// @ts-ignore
+const JobEntry = ({ jobID, jobs }) => {
+  // @ts-ignore
+  const JOB_DATA: any = jobs[jobID];
+
+  if (typeof JOB_DATA === 'undefined') {
+    return <></>;
+  }
+
+  return (
+    <div className='panel' style={{ minWidth: 200, width: '2em' }}>
+      <div className='body'>
+        <div className='header'>
+          <div className='view'>
+            <span
+              className={JOB_DATA.className}
+              style={{
+                display: 'flex',
+                alignContent: 'center',
+                gap: '1rem',
+              }}
+            >
+              <i className={JOB_DATA.iconName}></i>
+              <b style={{ fontSize: 18 }}>{JOB_DATA.name}</b>
+            </span>
+          </div>
+        </div>
+
+        <div className='divider'></div>
+
+        <div className='bar'>
+          <b>Level:</b> {JOB_DATA.level}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Jobs = () => {
+  // @ts-ignore
+  const { jobs } = useStore();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        flexFlow: 'wrap',
+      }}
+    >
+      {Object.keys(JOBS).map((jobID) => (
+        <JobEntry key={jobID} jobID={jobID} jobs={jobs} />
+      ))}
+    </div>
+  );
+};
