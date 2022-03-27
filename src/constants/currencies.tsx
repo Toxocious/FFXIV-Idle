98054a386d66d26f9a14523b9573942c8ac5a675
Jess <toxocious@gmail.com>
2022-03-26 17:42:07 -0700

    Fix constant currency imageName values

--- src/constants/currencies.tsx
+++ src/constants/currencies.tsx
@@ -2,56 +2,56 @@ export const CURRENCIES = {
   GIL: {
     name: 'Gil',
     value: 0,
-    imageName: 'gil',
+    imageName: 'src/assets/images/currency_gil.png',
   },
   MGP: {
     name: 'Minigame Points',
     value: 0,
-    imageName: 'mgp',
+    imageName: 'src/assets/images/currency_mgp.png',
   },
 
   SERPENT_SEALS: {
     name: 'Serpent Seals',
     value: 0,
-    imageName: 'serpent_seals',
+    imageName: 'src/assets/images/currency_serpent_seals.png',
   },
   VENTURES: {
     name: 'Ventures',
     value: 0,
-    imageName: 'ventures',
+    imageName: 'src/assets/images/currency_ventures.png',
   },
 
   ALLAGAN_TOMESTONE: {
     name: 'Allagan Tomestone of Poetics',
     value: 0,
-    imageName: 'tomestone_allagan',
+    imageName: 'src/assets/images/currency_tomestone_allagan.png',
   },
 
   SKYBUILDER_SCRIPS: {
     name: "Skybuilders' Scrip",
     value: 0,
-    imageName: 'skybuilder_scrip',
+    imageName: 'src/assets/images/currency_skybuilder_scrip.png',
   },
 
   CRAFTER_SCRIP_YELLOW: {
     name: "Yellow Crafter's Scrip",
     value: 0,
-    imageName: 'crafter_scrip_yellow',
+    imageName: 'src/assets/images/currency_crafter_scrip_yellow.png',
   },
   CRAFTER_SCRIP_WHITE: {
     name: "White Crafter's Scrip",
     value: 0,
-    imageName: 'crafter_scrip_white',
+    imageName: 'src/assets/images/currency_crafter_scrip_white.png',
   },
 
   GATHERER_SCRIP_YELLOW: {
     name: "Yellow Gatherer's Scrip",
     value: 0,
-    imageName: 'gatherer_scrip_yellow',
+    imageName: 'src/assets/images/currency_gatherer_scrip_yellow.png',
   },
   GATHERER_SCRIP_WHITE: {
     name: "White Gatherer's Scrip",
     value: 0,
-    imageName: 'gatherer_scrip_white',
+    imageName: 'src/assets/images/currency_gatherer_scrip_white.png',
   },
 };
