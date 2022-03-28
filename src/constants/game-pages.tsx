077faaa8083f2306e4ae44a26d6bd21b99400962
Jess <toxocious@gmail.com>
2022-03-27 17:19:07 -0700

    Implement array of navigation links

---
+++ src/constants/game-pages.tsx
@@ -0,0 +1,26 @@
+export const GAME_PAGES = [
+  {
+    id: 1,
+    name: 'Battle',
+    link: '/battle',
+    unlocked: true,
+  },
+  {
+    id: 2,
+    name: 'Gather',
+    link: '/gathering',
+    unlocked: true,
+  },
+  {
+    id: 3,
+    name: 'Craft',
+    link: '/crafting',
+    unlocked: true,
+  },
+  {
+    id: 4,
+    name: 'Changelog',
+    link: '/',
+    unlocked: true,
+  },
+];
