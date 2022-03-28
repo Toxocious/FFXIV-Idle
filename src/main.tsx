2a49a940c6cf61137081fb797dfeebc5750da690
Jess <toxocious@gmail.com>
2022-03-27 17:33:43 -0700

    Merge contents of App.tsx into Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { BrowserRouter } from 'react-router-dom';
 
 import { GameProvider } from './context/game-context';
+
+import { GameWindow } from './components/game-window';
+import { Navigation } from './components/navigation';
+import { Jobs } from './components/job-list';
+import { Currencies } from './components/currency-list';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <GameProvider>
-      <App />
+      <div className='App'>
+        <div className='background-image'></div>
+
+        <div className='container'>
+          <div className='game-container'>
+            <BrowserRouter>
+              <div className='left-sidebar-container'>
+                <Navigation />
+                <Jobs />
+              </div>
+              <GameWindow />
+              <Currencies />
+            </BrowserRouter>
+          </div>
+        </div>
+      </div>
     </GameProvider>
   </React.StrictMode>,
 
