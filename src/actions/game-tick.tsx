5d001dc57f8527c9d728c530b8abd41d238a1010
Jess <toxocious@gmail.com>
2022-03-31 18:13:47 -0700

    Only proc game ticks if an active job is set

--- src/actions/game-tick.tsx
+++ src/actions/game-tick.tsx
@@ -5,41 +5,38 @@ import { GetActiveJob } from '../utils/get-active-job';
 export const gameTick = ({ store, dispatch }, delta: number) => {
   delta = Math.round(delta * 10) / 10;
 
-  const pendingEffects: [] = [];
+  const PENDING_EFFECTS: [] = [];
 
-  const activePage: number = store.game.activePage;
-  console.log('[Game Tick] Active Page:', activePage);
-  switch (activePage) {
-    /**
-     * Battle
-     */
-    case 1:
-      console.log('[Game Tick] Processing Battle Page Data.');
+  const ACTIVE_PAGE: number = store.game.activePage;
+  const ACTIVE_JOB = Object.keys(store.jobs).filter(
+    (job: any) => store.jobs[job].active
+  );
 
-      break;
+  if (ACTIVE_JOB.length === 1) {
+    switch (ACTIVE_PAGE) {
+      /**
+       * Battle
+       */
+      case 1:
+        console.log('[Game Tick] Processing Battle Page Data.');
 
-    /**
-     * Gathering
-     */
-    case 2:
-      console.log('[Game Tick] Processing Gather Page Data.');
+        break;
 
-      break;
+      /**
+       * Gathering
+       */
+      case 2:
+        console.log('[Game Tick] Processing Gather Page Data.');
 
-    /**
-     * Crafting
-     */
-    case 3:
-      console.log('[Game Tick] Processing Crafting Page Data.');
+        break;
 
-      break;
+      /**
+       * Crafting
+       */
+      case 3:
+        console.log('[Game Tick] Processing Crafting Page Data.');
+
+        break;
+    }
   }
-
-  /**
-   * Process things that need to happen every tick.
-   *  - Automated battling
-   *  - Harvesting
-   *  - Crafting?
-   *    - Will this be automatic?
-   */
 };
