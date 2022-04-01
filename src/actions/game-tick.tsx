6f7856044e2276dfa95349a2ac69fe2770482c81
Jess <toxocious@gmail.com>
2022-03-31 18:43:50 -0700

    Limit page access to specific job types

--- src/actions/game-tick.tsx
+++ src/actions/game-tick.tsx
@@ -1,6 +1,3 @@
-import { useStore } from '../context/game-context';
-import { GetActiveJob } from '../utils/get-active-job';
-
 // @ts-ignore
 export const gameTick = ({ store, dispatch }, delta: number) => {
   delta = Math.round(delta * 10) / 10;
@@ -8,35 +5,45 @@ export const gameTick = ({ store, dispatch }, delta: number) => {
   const PENDING_EFFECTS: [] = [];
 
   const ACTIVE_PAGE: number = store.game.activePage;
-  const ACTIVE_JOB = Object.keys(store.jobs).filter(
+  const ACTIVE_JOB_ID = Object.keys(store.jobs).filter(
     (job: any) => store.jobs[job].active
   );
+  // @ts-ignore
+  const ACTIVE_JOB_DATA = store.jobs[ACTIVE_JOB_ID];
 
-  if (ACTIVE_JOB.length === 1) {
-    switch (ACTIVE_PAGE) {
-      /**
-       * Battle
-       */
-      case 1:
-        console.log('[Game Tick] Processing Battle Page Data.');
+  if (typeof ACTIVE_JOB_DATA === 'undefined') return;
 
-        break;
+  switch (ACTIVE_PAGE) {
+    /**
+     * Battle
+     */
+    case 1:
+      if (
+        ACTIVE_JOB_DATA.type !== 'DPS' &&
+        ACTIVE_JOB_DATA.type !== 'Tank' &&
+        ACTIVE_JOB_DATA.type !== 'Healer'
+      )
+        return;
+      console.log('[Game Tick] Processing Battle Page Data.');
 
-      /**
-       * Gathering
-       */
-      case 2:
-        console.log('[Game Tick] Processing Gather Page Data.');
+      break;
 
-        break;
+    /**
+     * Gathering
+     */
+    case 2:
+      if (ACTIVE_JOB_DATA.type !== 'Gatherer') return;
+      console.log('[Game Tick] Processing Gather Page Data.');
 
-      /**
-       * Crafting
-       */
-      case 3:
-        console.log('[Game Tick] Processing Crafting Page Data.');
+      break;
 
-        break;
-    }
+    /**
+     * Crafting
+     */
+    case 3:
+      if (ACTIVE_JOB_DATA.type !== 'Crafter') return;
+      console.log('[Game Tick] Processing Crafting Page Data.');
+
+      break;
   }
 };
