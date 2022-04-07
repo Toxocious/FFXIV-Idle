62bc33906a5dbbfbd2017812e6330631268ac334
Jess <toxocious@gmail.com>
2022-04-07 14:38:28 -0700

    Refactor level calc function to return additional level data

--- src/utils/calc-level.tsx
+++ src/utils/calc-level.tsx
@@ -1,9 +1,19 @@
 import { LEVEL_EXP } from '../constants/level-exp';
 
 export const CalcLevelFromExp = (exp: number = 0) => {
-  const LEVELS = Object.keys(LEVEL_EXP).filter(
+  const LEVEL: any = Object.keys(LEVEL_EXP).filter(
     (level: any) => LEVEL_EXP[level].totalAccumulatedExp < exp
   );
 
-  return LEVELS.slice(-1);
+  const CURRENT_LEVEL = parseInt(LEVEL.slice(-1));
+  const CURRENT_LEVEL_EXP_DATA: any = LEVEL_EXP[CURRENT_LEVEL];
+  const NEXT_LEVEL_EXP_DATA: any = LEVEL_EXP[CURRENT_LEVEL + 1];
+
+  return {
+    LEVEL: CURRENT_LEVEL,
+    EARNED_LEVEL_EXP: NEXT_LEVEL_EXP_DATA.totalAccumulatedExp - exp,
+    TOTAL_LEVEL_EXP:
+      NEXT_LEVEL_EXP_DATA.totalAccumulatedExp -
+      CURRENT_LEVEL_EXP_DATA.totalAccumulatedExp,
+  };
 };
