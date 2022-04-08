f7bc9c4f79c7930a8a3ff5e1743034672760a1fc
Jess <toxocious@gmail.com>
2022-04-08 16:18:17 -0700

    Reduce random generated enemy level range

--- src/utils/get-random-enemy.tsx
+++ src/utils/get-random-enemy.tsx
@@ -4,7 +4,7 @@ import { ENEMIES } from '../constants/enemies';
 export const GetRandomEnemy = (level: number) => {
   const POSSIBLE_ENEMIES: string[] = Object.keys(ENEMIES).filter(
     (enemy: any) =>
-      ENEMIES[enemy].level > level - 5 && ENEMIES[enemy].level < level + 5
+      ENEMIES[enemy].level > level - 2 && ENEMIES[enemy].level < level + 2
   );
 
   const RANDOM_ENEMY = Math.floor(Math.random() * POSSIBLE_ENEMIES.length);
