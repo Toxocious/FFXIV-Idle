c870ad9815a7f4e44c9d85ff995a6207da9e6c3b
Jess <toxocious@gmail.com>
2022-04-08 16:02:38 -0700

    Fix a bug where level calc failed if exp was 0

--- src/utils/calc-level.tsx
+++ src/utils/calc-level.tsx
@@ -1,9 +1,10 @@
 import { LEVEL_EXP } from '../constants/level-exp';
 
 export const CalcLevelFromExp = (exp: number = 0) => {
-  const LEVEL: any = Object.keys(LEVEL_EXP).filter(
-    (level: any) => LEVEL_EXP[level].totalAccumulatedExp < exp
-  );
+  const LEVEL: any =
+    Object.keys(LEVEL_EXP).filter(
+      (level: any) => LEVEL_EXP[level].totalAccumulatedExp <= exp
+    ) ?? 1;
 
   const CURRENT_LEVEL = parseInt(LEVEL.slice(-1));
   const CURRENT_LEVEL_EXP_DATA: any = LEVEL_EXP[CURRENT_LEVEL];
