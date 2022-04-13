e9362dff8586690a6345d677b057f10a05388622
Jess <toxocious@gmail.com>
2022-04-12 21:21:40 -0700

    Display all owned items in the inventory route

--- src/routes/inventory/index.tsx
+++ src/routes/inventory/index.tsx
@@ -1,8 +1,33 @@
+import { useStore } from '../../providers/game';
+
+import { Slot } from '../../components/slot';
+
 export function InventoryWindow() {
+  const { items } = useStore();
+  console.log('[Inventory] Items:', items);
+
   return (
     <>
       <h2>Inventory</h2>
       <br />
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          gap: '1em 0.9em',
+        }}
+      >
+        {Object.keys(items).map((item) => (
+          <Slot
+            index={item}
+            amount={items[item].amount}
+            displayDropChance={false}
+          />
+        ))}
+      </div>
     </>
   );
 }
