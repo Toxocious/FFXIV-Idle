ac748efe72d4a3552cf40370d8f163245337ad62
Jess <toxocious@gmail.com>
2022-04-16 18:51:24 -0700

    Display the player's stats on the stat page

--- src/routes/stats/index.tsx
+++ src/routes/stats/index.tsx
@@ -1,7 +1,21 @@
+import { useStore } from '../../providers/game';
+import { STATS } from '../../constants/stats';
+
 export function StatsWindow() {
+  const { stats } = useStore();
+  console.log('[Stats] Your Stats:', stats, Object.keys(stats));
+
   return (
     <>
       <h2>Stats</h2>
+
+      <div>
+        {Object.keys(stats).map((stat) => (
+          <div key={stat} data-stat-key={stat} style={{ fontSize: 16 }}>
+            <b>{STATS[stat].name}</b>: {STATS[stat].amount.toLocaleString()}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
