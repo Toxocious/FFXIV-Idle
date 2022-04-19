b60e78b5d3de63bfff0161935393276f7bb545ab
Jess <toxocious@gmail.com>
2022-04-19 14:17:53 -0700

    Set up the 'Gather' page route contents

--- src/routes/gathering/index.tsx
+++ src/routes/gathering/index.tsx
@@ -1,7 +1,15 @@
+import { GatherNode } from '../../components/gather-node';
+
+import './index.css';
+
 export function GatheringWindow() {
   return (
     <>
       <h2>Gathering Window</h2>
+
+      <div className='gathering-container'>
+        <GatherNode />
+      </div>
     </>
   );
 }
